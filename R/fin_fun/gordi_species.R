01dd54213dfd6ec7facafd757c394800406f91fa
BranoKopcan <kopcanb@gmail.com>
2025-08-19 13:02:16 +0200

    added shortcuts

--- R/fin_fun/gordi_species.R
+++ R/fin_fun/gordi_species.R
@@ -10,6 +10,9 @@ gordi_species <- function(pass,
                           linetype = '',
                           linewidth = '',
                           arrow_size = '',
+                          shortcut = '',
+                          shortcut_length = 3, 
+                          shortcut_colour = '',
                           repel_label = T) {
   
   ### axis names used in spe_df 
@@ -177,9 +180,103 @@ gordi_species <- function(pass,
   
   
   
+  #'shortcuts
+  if(!identical(shortcut, '')){
+    #' split species  into individual tokens
+    parts_list <- str_split(spe_df[[1]], '\\s')
+    #' remove tokens like Sect., sect.... 
+    rank_tokens <- c('sect\\.', 'Sect\\.', 'cf\\.')
+    rx_drop <- regex(paste0('^(', paste(rank_tokens,  collapse = '|'), ')$'))
+    parts_list <- lapply(parts_list, function (x) x[!str_detect(x, rx_drop)])
+    #' detect subspecies and take epithet after it
+    rx_sub <- regex('^(subsp\\.|ssp\\.)$')
+    has_sub <- vapply(parts_list, function (x) any(str_detect(x, rx_sub)), logical (1))
+    #'individual genus, species, subspecies
+    genus <- map_chr(parts_list, 1)
+    epithet <- map_chr(parts_list, 2)
+    subsp <- vapply(parts_list, function (x) { i <- match(TRUE, str_detect(x, rx_sub))
+    x[i + 1L]}, character(1))
+    #'shortcuts based on shortcut_length
+    gN <- str_sub(genus, 1, shortcut_length)
+    sN <- str_sub(epithet, 1, shortcut_length)
+    subN <- str_sub(subsp, 1, shortcut_length)
+    
+    if(shortcut == 'upper.lower') {
+      short_non <- str_c(str_to_title(gN), str_to_lower(sN), sep = '.')
+      short_sub <- str_c(str_to_title(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '.')
+    } else if(shortcut == 'lower.lower'){
+      short_non <- str_c(str_to_lower(gN), str_to_lower(sN), sep = '.')
+      short_sub <- str_c(str_to_lower(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '.')
+    } else if(shortcut == 'upper.upper'){
+      short_non <- str_c(str_to_title(gN), str_to_title(sN), sep = '.')
+      short_sub <- str_c(str_to_title(gN), str_to_title(sN), 'ssp', str_to_title(subN), sep = '.')
+    } else if(shortcut == 'upperupper'){
+      short_non <- str_c(str_to_title(gN), str_to_title(sN), sep = '')
+      short_sub <- str_c(str_to_title(gN), str_to_title(sN), 'ssp', str_to_title(subN), sep = '')
+    } else if(shortcut == 'upper_lower'){
+      short_non <- str_c(str_to_title(gN), str_to_lower(sN), sep = '_')
+      short_sub <- str_c(str_to_title(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '_')
+    } else if(shortcut == 'lower_lower'){
+      short_non <- str_c(str_to_lower(gN), str_to_lower(sN), sep = '_')
+      short_sub <- str_c(str_to_lower(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '_')
+    } else if(shortcut == 'upper_upper'){
+      short_non <- str_c(str_to_title(gN), str_to_title(sN), sep = '_')
+      short_sub <- str_c(str_to_title(gN), str_to_title(sN), 'ssp', str_to_title(subN), sep = '_')
+    } else if(shortcut == 'upper*lower'){
+      short_non <- str_c(str_to_title(gN), str_to_lower(sN), sep = '*')
+      short_sub <- str_c(str_to_title(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '*')
+    } else if(shortcut == 'lower*lower'){
+      short_non <- str_c(str_to_lower(gN), str_to_lower(sN), sep = '*')
+      short_sub <- str_c(str_to_lower(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '*')
+    } else if(shortcut == 'upper*upper'){
+      short_non <- str_c(str_to_title(gN), str_to_title(sN), sep = '*')
+      short_sub <- str_c(str_to_title(gN), str_to_title(sN), 'ssp', str_to_title(subN), sep = '*')
+    } else if(shortcut == 'upper-lower'){
+      short_non <- str_c(str_to_title(gN), str_to_lower(sN), sep = '-')
+      short_sub <- str_c(str_to_title(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '-')
+    } else if(shortcut == 'lower-lower'){
+      short_non <- str_c(str_to_lower(gN), str_to_lower(sN), sep = '-')
+      short_sub <- str_c(str_to_lower(gN), str_to_lower(sN), 'ssp', str_to_lower(subN), sep = '-')
+    } else if(shortcut == 'upper-upper'){
+      short_non <- str_c(str_to_title(gN), str_to_title(sN), sep = '-')
+      short_sub <- str_c(str_to_title(gN), str_to_title(sN), 'ssp', str_to_title(subN), sep = '-')
+    } else {
+      warning("Unknown 'shortcut': ", shortcut, ' -> No short name created.')
+    }
+  }
+  
+  #' creates tibble with short names if subspecies is non existent it takes short_non otherwise short_sub
+  spe_df <- spe_df|>
+    mutate(short_name = ifelse(has_sub, short_sub, short_non))
+  
+  map_shortcut_colour <- !identical(shortcut_colour, '') && has_name(spe_df, shortcut_colour)
+  const_shortcut_colour <- !identical(shortcut_colour, '') && !map_shortcut_colour && (grepl("^#(?:[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", shortcut_colour) || shortcut_colour %in% grDevices::colours())
+  
+  
   #' accounting for labeling
-  if (label != '') {
-    if (repel_label) {
+  if (identical(label, '') && !identical(shortcut, '')) {
+    if (map_shortcut_colour){
+      p <- p + ggnewscale::new_scale_colour()
+    }
+    map_args_text <- if (map_shortcut_colour){
+      aes(Axis_spe1, Axis_spe2, label = short_name, colour = !!sym(shortcut_colour))
+    } else {
+      aes(Axis_spe1, Axis_spe2, label = short_name)
+    }
+    const_args_text <- list()
+    if (!map_shortcut_colour){
+      if(const_shortcut_colour){
+        const_args_text$colour <- shortcut_colour
+      } else {const_args_text$colour <- 'black'}
+    }
+    
+    if (isTRUE(repel_label)) {
+      p <- p + do.call(geom_text_repel, c(list(data = spe_df, mapping = map_args_text), const_args_text))
+    } else {
+      p <- p + do.call(geom_text, c(list(data = spe_df, mapping = map_args_text), const_args_text))
+    }
+  } else if (!identical(label, '') && identical (shortcut, '')){
+    if (isTRUE(repel_label)){
       p <- p + geom_text_repel(data = spe_df, aes(Axis_spe1, Axis_spe2, label = !!sym(label)))
     } else {
       p <- p + geom_text(data = spe_df, aes(Axis_spe1, Axis_spe2, label = !!sym(label)))
