31e48785606a3d51d75a3d79ea88eb13ee2a42f0
Stepanka Pustkova <st.pustkova@gmail.com>
2025-08-22 18:48:37 +0200

    updates in all gordi_functions regarding actual_labs, and als gordi_read is now ready with the documentation

--- R/fin_fun/gordi_read.R
+++ R/fin_fun/gordi_read.R
@@ -1,6 +1,51 @@
-# gordi_read()
+#' Read ordination output into a standardized structure
+#' 
+#' @description
+#' #' `gordi_read` is the first function in the family of `gordi_`functions. 
+#' It takes the output of `rda`, `cca`, `capscale`, `decorana` and `metaMDS` and 
+#' translates in into a format usable by other `gordi_` functions.
+#'
+#'
+#' @param m An ordination result: object of class `rda`, `cca`, `capscale`,
+#'   `decorana`, or `metaMDS`
+#' @param env A data frame with environmental variables of samples; must have
+#'   the same number of samples (rows) as the species table
+#' @param traits A data frame of species traits; one species can have only one
+#'   trait value in its dedicated column
+#' @param choices Numeric vector of selected ordination axes; defaults to the
+#'   first two axes
+#' @param scaling Scaling of species and site scores; see `?scores.cca` or
+#'   related functions
+#' @param correlation Logical; default = F, see `?scores.cca` or `?scores.rda` for details
+#' @param hill Logical; default = F, see `?scores.cca` for details
+#' 
+#' 
+#' @return A list with elements:
+#'   \describe{
+#'     \item{m}{the original ordination object}
+#'     \item{explained_variation}{eigenvalues divided by total inertia, or NA for DCA/NMDS}
+#'     \item{site_scores}{tibble of site (sample) scores}
+#'     \item{species_scores}{tibble of species scores}
+#'     \item{predictor_scores}{tibble of biplot scores for environmental predictors}
+#'     \item{env}{environmental data frame (if supplied)}
+#'     \item{traits}{trait data frame (if supplied)}
+#'     \item{choices}{selected axes}
+#'     \item{type}{character string with ordination type}
+#'     \item{axis_names}{axis labels}
+#'     \item{species_names}{species names}
+#'     \item{predictor_names}{predictor names, or NA for DCA/NMDS}
+#'   }
+#' 
+#' @examples
+#' library(vegan)
+#' library(tidyverse)
+#' data(dune)
+#' data(dune.env)
+#' mod <- rda(dune ~ ., data = dune.env)
+#' res <- gordi_read(mod, env = dune.env)
+#' str(res)
 
-# arguments
+
 gordi_read <- function(m,
                        env = NULL,
                        traits = NULL,
@@ -17,23 +62,26 @@ gordi_read <- function(m,
     inherits(m, 'rda')                                                   ~ 'RDA',
     inherits(m, 'cca') & is.null(m$call$distance) & is.null(m$CCA)       ~ 'CA',
     inherits(m, 'cca')                                                   ~ 'CCA',
+    inherits(m, 'decorana')                                              ~ 'DCA',
+    inherits(m, c('metaMDS', 'monoMDS'))                                 ~ 'NMDS',
     TRUE ~ paste(class(m), collapse = '/') # writes just one output
   )
 
   # create pass object 
   pass <- list(
     m = m,
-    explained_variation = m$CA$eig/m$tot.chi,
-    site_scores = as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$sites)),
-    species_scores = as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$species)),
+    explained_variation = if (type %in% c('DCA', 'NMDS')) {NA} else {eigenvals(m) / m$tot.chi},
+    site_scores = as_tibble(as.data.frame(scores(m, display = 'sites', scaling = scaling, choices = choices, correlation = correlation, hill = hill))),
+    species_scores = as_tibble(as.data.frame(scores(m, display = 'species', scaling = scaling, choices = choices, correlation = correlation, hill = hill))),
     predictor_scores = as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$biplot)),
     env = env,
     traits = traits,
     choices = choices,
     type = type,
+    axis_names = colnames(scores(m, display = 'sites')),
     species_names = as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$species), rownames = 'species_names')[1],
-    predictor_names = as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$biplot), rownames = 'predictor_names')[1]  
-  )
+    predictor_names = if (type %in% c('DCA', 'NMDS')) {NA} else {as_tibble(as.data.frame(scores(m, scaling = scaling, choices = choices, correlation = correlation, hill = hill)$biplot), rownames = 'predictor_names')[1]}
+    )
   
   # Return pass object
   return(pass)
